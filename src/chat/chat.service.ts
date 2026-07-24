65b66d60417f4401dcedd446f889d6dfc4251e1f
Aliyan Sajid <hammadalimughal08@gmail.com>
2026-07-25 01:13:41 +0500

    Chat issue

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -175,7 +175,8 @@ export class ChatService {
       );
     }
 
-    const { page = 1, limit = 10 } = paginationDto;
+    const page = Number(paginationDto.page) || 1;
+    const limit = Number(paginationDto.limit) || 10;
     const skip = (page - 1) * limit;
 
     const [data, total] = await Promise.all([
